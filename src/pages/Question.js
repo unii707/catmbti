74fd4a8466804f8710e6ea92699afaa86d797ed2
unii707 <joehuni@naver.com>
2022-12-15 15:56:20 +0900

    add home page

--- src/pages/Question.js
+++ src/pages/Question.js
@@ -1,20 +1,68 @@
 import React from 'react';
 import styled from 'styled-components';
+import { useNavigate } from 'react-router-dom';
 import { Button, ProgressBar } from 'react-bootstrap';
 import { QuestionData } from '../assets/data/questiondata';
 
+
 const Question = () => {
+    const [questionNo, setQuestionNo] = React.useState(0);
+    const [totalScore, setTotalScore] = React.useState([
+        { id: "EI", score: 0 },
+        { id: "SN", score: 0 },
+        { id: "TF", score: 0 },
+        { id: "JP", score: 0 },
+    ])
+    const navigate = useNavigate();
+
+    console.log('totalScore', totalScore);
+
+    const handleClickButton = (no, type) => {
+
+        const newScore = totalScore.map((s) =>
+            s.id === type ? { id: s.id, score: s.score + no } : s
+        );
+        
+        setTotalScore(newScore);
+
+        if (QuestionData.length !== questionNo + 1) {
+            setQuestionNo(questionNo + 1);
+        } else {
+            navigate ("/result");
+        }
+ 
+
+        // if (type === "EI") {
+        //     const addScore = totalScore[0].score + no;
+        //     const newObject = { id: "EI", score: addScore };
+        //     totalScore.splice(0, 1, newObject);    
+        // }else if (type === "SN") {
+        //     const addScore = totalScore[1].score + no;
+        //     const newObject = { id: "SN", score: addScore };
+        //     totalScore.splice(1, 1, newObject);
+        // } else if (type === "TF") {
+        //     const addScore = totalScore[2].score + no;
+        //     const newObject = { id: "TF", score: addScore };
+        //     totalScore.splice(2, 1, newObject);
+        // } else {
+        //     const addScore = totalScore[3].score + no;
+        //     const newObject = { id: "JP", score: addScore };
+        //     totalScore.splice(3, 1, newObject);
+        // }
+
+    }
+
     return (
         <Wrapper>
-            <ProgressBar striped variant="info" now={20} style={ {marginTop: "20px"}} />
+            <ProgressBar variant="info" now={(questionNo / QuestionData.length) * 100 } style={ {marginTop: "20px"}} />
             <Contents>
-                <Title>{QuestionData[0].title}</Title>
+                <Title>{QuestionData[questionNo].title}</Title>
                 <ButtonGroup>
-                    <Button style={{ width: "40%", minHeight: "200px", fontSize: "15pt" }}>
-                        {QuestionData[0].answera}
+                    <Button onClick= {()=>handleClickButton(1, QuestionData[questionNo].type) } style={{ width: "40%", minHeight: "200px", fontSize: "15pt" }}>
+                        {QuestionData[questionNo].answera}
                     </Button>
-                    <Button style={{ width: "40%", minHeight: "200px", fontSize: "15pt", marginLeft: "20px" }} >
-                        {QuestionData[0].answerb}
+                    <Button onClick= {()=>handleClickButton(0, QuestionData[questionNo].type) }style={{ width: "40%", minHeight: "200px", fontSize: "15pt", marginLeft: "20px" }} >
+                        {QuestionData[questionNo].answerb}
                     </Button>                   
                 </ButtonGroup>
 
