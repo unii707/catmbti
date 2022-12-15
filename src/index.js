97831f064cc46fbe424dc6e6d22bdf6c5eb31237
unii707 <joehuni@naver.com>
2022-12-15 17:46:24 +0900

    add home page

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import { createRoot } from "react-dom/client";
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
 
 const rootElement = document.getElementById('root');
